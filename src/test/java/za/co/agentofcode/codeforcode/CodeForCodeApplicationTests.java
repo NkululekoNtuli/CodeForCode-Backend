626b481a3565062e9e359da85ba8dfe5eca6123b
Nkululeko <nkntulijhb024@student.wethinkcode.co.za>
2025-08-29 12:33:15 +0200

    initial commit

---
+++ src/test/java/za/co/agentofcode/codeforcode/CodeForCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.agentofcode.codeforcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeForCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
