2b2e60e644d7d55dc54bd7f484a5eabcc8e60b28
Nkululeko_from_linux_os <nkntulijhb024@student.wethinkcode.co.za>
2025-09-10 21:10:24 +0200

    subm... entity complete

--- src/main/java/za/co/agentofcode/codeforcode/model/Submission.java
+++ src/main/java/za/co/agentofcode/codeforcode/model/Submission.java
@@ -1,4 +1,27 @@
 package za.co.agentofcode.codeforcode.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import org.apache.logging.log4j.CloseableThreadContext;
+
+import java.time.Instant;
+
+@Entity
 public class Submission {
+    @Id @GeneratedValue
+    private Long id;
+    @ManyToOne
+    private Match match;
+    @ManyToOne
+    private User user;
+    private String language;
+    private String code;
+    private boolean verdict;
+    private int runtimeMs;
+    private int memoryKb;
+    private int ingenuity;
+    private int score;
+    private Instant submissionTime;
 }
