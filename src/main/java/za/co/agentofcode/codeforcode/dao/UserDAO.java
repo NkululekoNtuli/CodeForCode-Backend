fa5aa78f44287dba0e3a9180e561cb4a1cca768e
Nkululeko_from_linux_os <nkntulijhb024@student.wethinkcode.co.za>
2025-09-10 22:45:29 +0200

    cleaning

--- src/main/java/za/co/agentofcode/codeforcode/dao/UserDAO.java
+++
@@ -1,4 +0,0 @@
-package za.co.agentofcode.codeforcode.dao;
-
-public class UserDAO {
-}
--- src/main/java/za/co/agentofcode/codeforcode/model/Submission.java
+++ src/main/java/za/co/agentofcode/codeforcode/model/Submission.java
@@ -4,7 +4,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
-import org.apache.logging.log4j.CloseableThreadContext;
 
 import java.time.Instant;
 
